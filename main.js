17f46f1b833054da0de5a9bb9b71c3173c5bbce0
Poohr <nao_nao_12_23@yahoo.co.jp>
2021-03-19 07:12:05 +0900

    [update]追加ボタンをクリックするとアラートが表示

--- main.js
+++ main.js
@@ -2,5 +2,16 @@
 
 const addButton = document.querySelector('#addButton');
 addButton.addEventListener('click', () => {
-    alert('add button clicked');
+    showAlert('追加ボタンクリック', 'これで確認できました。', 'もう閉じて良いですよ');
 });
+
+function showAlert(header, subHeader, message) {
+    const alert = document.createElement('ion-alert');
+    alert.header = header;
+    alert.subHeader = subHeader;
+    alert.message = message;
+    alert.buttons = ['OK'];
+
+    document.body.appendChild(alert);
+    return alert.present();
+}
